930d4b8ca2056ad9ba9e77dfbddcf9aa4cfad6a1
Akash Badhe <akashb@arista.com>
2024-04-08 10:30:28 +0530

    feat(router): add react router
    - Added react router
    - add test components
    - updated my-lib to file path in package.json

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from "my-lib";
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is the About page.</p>
+            <Button label="About Button"></Button>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from "my-lib";
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to Home Page</h1>
+            <p>This is the home page of our application.</p>
+            <Button label="Home Button"></Button>
+        </div>
+    );
+}
+
+export default Home;
